f5c613532b273be8d227f5ec6d5b7036441b5cc6
dadu0699 <dadu0699@hotmail.com>
2021-09-06 22:19:53 -0600

    [UPDATE] Applying programming styles

--- Backend/controllers/friendship.controller.js
+++ Backend/controllers/friendship.controller.js
@@ -1,39 +1,37 @@
 const friendshipModel = require('../models/friendship.model');
 
-const friendshipController = {
-  create: (req, res) => {
-    friendshipModel.create(req.body, (err, results) => {
-      if (err) {
-        res.status(500).send({
-          code: 500,
-          data: err
-        });
-        return;
-      }
-
-      res.status(200).send({
-        code: 200,
-        data: results
+function create(req, res) {
+  friendshipModel.create(req.body, (err, results) => {
+    if (err) {
+      res.status(500).send({
+        code: 500,
+        data: err
       });
+      return;
+    }
+
+    res.status(200).send({
+      code: 200,
+      data: results
     });
-  },
+  });
+}
 
-  get: (req, res) => {
-    friendshipModel.get(req.query, (err, results) => {
-      if (err) {
-        res.status(500).send({
-          code: 500,
-          data: err
-        });
-        return;
-      }
-
-      res.status(200).send({
-        code: 200,
-        data: results
+function get(req, res) {
+  friendshipModel.get(req.query, (err, results) => {
+    if (err) {
+      res.status(500).send({
+        code: 500,
+        data: err
       });
-    });
-  },
-};
+      return;
+    }
 
-module.exports = friendshipController;
+    res.status(200).send({
+      code: 200,
+      data: results
+    });
+  });
+}
+
+module.exports = { create, get };
