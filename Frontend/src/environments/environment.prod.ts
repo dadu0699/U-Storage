094bfd56c593ad2447a3cda37953e636530a6e34
dadu0699 <dadu0699@hotmail.com>
2021-09-10 08:22:23 -0600

    [UPDATE] Production Server EndPoint

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: 'http://192.168.0.4:2800',
+  url: 'http://loadbalancer-38pareja-p1-1586470045.us-east-2.elb.amazonaws.com',
   bucket: 'https://archivos-38pareja-p1.s3.us-east-2.amazonaws.com',
 };
