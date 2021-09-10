e5039995a1bcf7cde4b4f2a5730f2ddb3ef357f2
dadu0699 <dadu0699@hotmail.com>
2021-09-10 11:06:45 -0600

    [ADD] AWS config

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  url: 'http://localhost:3000',
+  url: 'http://loadbalancer-38pareja-p1-1586470045.us-east-2.elb.amazonaws.com',
   bucket: 'https://archivos-38pareja-p1.s3.us-east-2.amazonaws.com',
 };
